1889f5b6d2647721d5db87a3938f6fd0cf81241e
XIAO100he <xiao100he@gmil.com>
2020-11-17 19:29:14 +0900

    Firebaseの設定、不要ファイルを削除

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDbAeL03qKnQm04l7iCMrCDA1pghihi_PQ",
+  authDomain: "hawks-ec-app.firebaseapp.com",
+  databaseURL: "https://hawks-ec-app.firebaseio.com",
+  projectId: "hawks-ec-app",
+  storageBucket: "hawks-ec-app.appspot.com",
+  messagingSenderId: "322015615964",
+  appId: "1:322015615964:web:7fa8ab301870f0c8098d19",
+  measurementId: "G-RD5BP5BTE3"
+};
